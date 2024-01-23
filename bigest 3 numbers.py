2fce2fae2d8a84c0426708c67b2464590efa6a78
Rahul pt <146359399+Rahulpt002@users.noreply.github.com>
2024-01-23 11:16:41 +0000

    new update

--- bigest 3 numbers.py
+++ bigest 3 numbers.py
@@ -1,6 +1,6 @@
 a=int(input('enter 1st number'))
-b=int(input('enter 1st number'))
-c=int(input('enter 1st number'))
+b=int(input('enter 2nd number'))
+c=int(input('enter 3rd number'))
 if(a>b)and(a>c):
     print('a is greatest')
 elif(b>a)and(b>c):
---
+++ myDict = {}.py
@@ -0,0 +1,4 @@
+myDict = {'a':1,'b':2,'k':[]}
+lst = ['Geeks', 'For', 'Geeks']
+myDict['k'].append(lst)
+print(myDict)
